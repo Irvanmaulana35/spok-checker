ea411f39b48c2c02dfd560481a51fb8f762f05c8
Irvanmaulana35 <irfan.maulan.im47@gmail.com>
2022-06-11 07:51:39 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,11 @@
         <h1>Dashboard</h1>
         <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="#">Layout</a></div>
-            <div class="breadcrumb-item">Default Layout</div>
-        </div>
+                    </div>
     </x-slot>
 
     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
         <x-jet-welcome />
     </div>
 </x-app-layout>
+
